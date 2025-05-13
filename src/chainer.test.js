c069d21a4dc40ede1e4d4f837aa1abcb899d0483
Alenalenk <alena.lenk.worku@gmail.com>
2025-05-13 12:31:02 +0300

    add tests

--- src/chainer.test.js
+++ src/chainer.test.js
@@ -1,3 +1,36 @@
-'use strict'
+'use strict';
+/*
+function f1(x) {return x * 2};
+function f2(x) {return x + 2};
+function f3(x) {return Math.pow(x, 2)}; */
 
-const { chainer } = require('./chainer');
+describe('chainer', () => {
+  const { chainer } = require('./chainer');
+
+  it(`should be declared`, () => {
+    expect(chainer).toBeInstanceOf(Function);
+  });
+
+  it(`should return a function that does a left
+fold on the given functions`, () => {
+    const f1 = jest.fn((x) => x * 2);
+    const f2 = jest.fn((x) => x + 2);
+    const f3 = jest.fn((x) => Math.pow(x, 2));
+
+    chainer([f1, f2, f3])(0);
+
+    expect(f1).toHaveBeenCalled();
+    expect(f2).toHaveBeenCalled();
+    expect(f3).toHaveBeenCalled();
+  });
+
+  it(`should return the correct result`, () => {
+    const f1 = jest.fn((x) => x * 2);
+    const f2 = jest.fn((x) => x + 2);
+    const f3 = jest.fn((x) => Math.pow(x, 2));
+
+    const result = chainer([f1, f2, f3])(0);
+
+    expect(result).toBe(4);
+  });
+});
